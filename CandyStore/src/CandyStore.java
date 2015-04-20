a4b1092297d8abe326cfe370f4bcfceb782f5994
theGANOUSH <babaganoush45@gmail.com>
2015-04-19 22:01:40 -0400

    Added knapsack method

--- CandyStore/src/CandyStore.java
+++ CandyStore/src/CandyStore.java
@@ -1,48 +1,53 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.Scanner;
 
 public class CandyStore {
+	
+	static int[] knapsack(Scanner input, int n, int m)
+	{
+		int[] nKnapsack = new int[m + 1];
 
-	public static void main(String[] args) throws FileNotFoundException {
+
+		for (int i=0; i<n; i++)
+		{
+
+			int calories = input.nextInt();
+			int cCost = (int)(input.nextDouble()*100+.01);
+
+			for (int j=cCost; j<nKnapsack.length; j++)
+			{
+				nKnapsack[j] = Math.max(nKnapsack[j], nKnapsack[j-cCost]+calories);
+			}
+		}
 		
-		  
-        //creating File instance to reference text file in Java
+		return nKnapsack;
+	}
+	
+	
+
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		
         File fInput = new File("Input.txt");
       
 		Scanner scan = new Scanner(fInput);
-
-		int n = scan.nextInt();
+		 
+		int nCandies = scan.nextInt();
 		
-		int price = (int)(scan.nextDouble()*100+.01);
-
-		while (n != 0)
+		int tCents = (int)(scan.nextDouble()*100+.01);
+		
+		while (nCandies != 0)
 		{
-
-			// Run the DP in cents.
-			int[] dp = new int[price+1];
-
-			// Loop through each item
-			for (int i=0; i<n; i++)
-			{
-
-				// Get this info.
-				int cal = scan.nextInt();
-				int thisPrice = (int)(scan.nextDouble()*100+.01);
-
-				// Run loop forward to allow to take as many of this candy as possible.
-				for (int j=thisPrice; j<dp.length; j++)
-					dp[j] = Math.max(dp[j], dp[j-thisPrice]+cal);
-			}
-
-			// Print the answer and go to the next case.
-			System.out.println(dp[dp.length-1]);
-			n = scan.nextInt();
-			price = (int)(scan.nextDouble()*100+.01);
+			int[] nKnapsack = knapsack(scan, nCandies, tCents);
+			
+			System.out.println(nKnapsack[nKnapsack.length-1]);
+			
+			nCandies = scan.nextInt();
+			tCents = (int)(scan.nextDouble()*100+.01);
+			
 		}
-		
-		
 		
 		scan.close();
 	}
